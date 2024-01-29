c2e64b7354252fb96a1d50e3c465f989fe653659
unknown <horainlee87@gmail.com>
2024-01-29 10:50:08 +0900

    회원가입, 로그인 구현

--- api/account/urls.py
+++ api/account/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import UserListView, UserSignUpView
+from .views import UserListView, UserSignUpView, UserLogInView
 
 namespace = "account"
 
 urlpatterns = [
     path("list/", UserListView.as_view()),
     path("signup/", UserSignUpView.as_view()),
+    path("login/", UserLogInView.as_view()),
 ]
--- api/config/settings.py
+++ api/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "rest_framework_simplejwt",
     'account',  
     'plans',
     'places',
@@ -70,6 +72,13 @@ TEMPLATES = [
         },
     },
 ]
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+}
 
 WSGI_APPLICATION = "config.wsgi.application"
 

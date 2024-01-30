0b5e1def699a5109b9c00f3d3e7c5862adc6a7d1
dlgksqls <horainlee87@gmail.com>
2024-01-30 16:05:37 +0900

    manager와 user 구분

---
+++ api/places/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from .models import Place, Tag, Review
+
+
+class TagModelSerializer(ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
+    def create(self, data):
+        tag_name = data.get("name")
+
+        if "#" not in tag_name:
+            tag_name = "#" + tag_name
+        data["name"] = tag_name
+
+        return super().create(data)
+
+
+class ReviewModelSerializer(ModelSerializer):
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+class PlaceModelSerializer(ModelSerializer):
+    ## 장고에서 자동으로  related_name 을 review_set으로 설정해줌
+    reviews = ReviewModelSerializer(source="review_set", read_only=True, many=True)
+
+    class Meta:
+        model = Place
+        fields = ["name", "image", "info", "address", "like", "tag", "reviews"]

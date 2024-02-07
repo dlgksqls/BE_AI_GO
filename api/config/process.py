dbc48952e9eb1a56d30cbacfac4f221e6829210e
unknown <horainlee87@gmail.com>
2024-02-07 15:54:19 +0900

    orm 사용하여 csv파일 db로 넣기 고민해보기

--- api/config/process.py
+++ api/config/process.py
@@ -1,30 +1,24 @@
 import pandas as pd
-import sqlite3
+from places.models import Place, Tag
 
 # Read csv file.
 df = pd.read_csv(r"C:\Users\user\Desktop\BE_AI_GO\api\config\202309.csv", encoding='cp949')
 
+hardness = df["hardness"]
+latitude = df["latitude"]
+
 # Connect to (create) database.
-database = r"C:\Users\user\Desktop\BE_AI_GO\api\db.sqlite3"
-conn = sqlite3.connect(database)
-dtype={
-    "name": "CharField",
-    "image": "ImageField",
-    "branch": "CharField", 
-    "classification": "CharField",
-    "industry_classification": "CharField",
-    "city_county": "CharField",
-    "Administrative": "CharField", 
-    "legal": "CharField",
-    "adress": "TextField",
-    "building": "CharField",
-    "street_name_address": "TextField", 
-    "zip_code": "CharField",
-    "floor": "IntegerField",
-    "hardnesss": "FloatField",
-    "latitude": "FloatField", 
-    "info": "TextField",
-    "like": "IntegerField",
-}
-df.to_sql(name='places_place', con=conn, if_exists='replace', dtype=dtype, index=True, index_label="id")
-conn.close()
+for index, row in df.iterrows():
+    tag, created = Tag.objects.get_or_create(name = row['tag'])
+    place = Place.objects.create(
+        name=row['name'],
+        image=row['image'],
+        classification=row['classification'],
+        street_name_address=row['street_name_address'],
+        hardness=row['hardness'],
+        latitude=row['latitude'],
+        info=row['info'],
+        like=row['like'],
+        tag=tag
+    )
+    place.save()

849d0e33a2da075c2e1cead9c2c83bf7fae5ddb2
unknown <horainlee87@gmail.com>
2024-02-12 10:14:44 +0900

    jwt 인증 통한 user권한 별 장소 조회

--- api/places/views.py
+++ api/places/views.py
@@ -4,6 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.decorators import api_view, action
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Place, Tag, Review
 from .serializers import PlaceModelSerializer, TagModelSerializer, ReviewModelSerializer
@@ -14,14 +15,15 @@ from .serializers import PlaceModelSerializer, TagModelSerializer, ReviewModelSe
 class PlaceView(ModelViewSet):
     serializer_class = PlaceModelSerializer
     queryset = Place.objects.all()
-    
+    authentication_classes = [JWTAuthentication]
+
     def get_permissions(self):
         permission_classes = list()
         action = self.action
         
-        if action == 'list':
+        if action == 'list' or action == 'retrieve':
             permission_classes = [AllowAny]
-        elif action in ['create', 'retrieve', 'update', 'partial_update', 'destroy']:
+        elif action in ['create' , 'update', 'partial_update', 'destroy']:
             permission_classes = [IsAdminUser]
         return [permission() for permission in permission_classes]
     

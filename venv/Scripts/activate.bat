8b3ad90a43a7de270eeb0717e80c8a74172ef008
unknown <horainlee87@gmail.com>
2024-02-29 15:12:38 +0900

    배포 연습

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\Desktop\BE_AI_GO\venv
+set VIRTUAL_ENV=C:\Users\user\Desktop\test\BE_AI_GO\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
